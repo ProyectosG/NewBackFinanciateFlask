85b59b046fa471436969a3188c184159f7d1b5f3
Enrique Lopez <95664476+ingenriquelopez@users.noreply.github.com>
2025-04-05 20:57:32 +0000

    test07

--- src/resources/usuarios.py
+++ src/resources/usuarios.py
@@ -116,7 +116,7 @@ def obtener_usuarios():
 @jwt_required()
 def actualizar_usuario():
     usuario_id = get_jwt_identity()  # Obtiene el ID del usuario autenticado desde el token
-    usuario = Usuario.query.get_or_404(usuario_id)  # Si no existe, devuelve un error 404
+    #usuario = Usuario.query.get_or_404(usuario_id)  # Si no existe, devuelve un error 404
 
     data = request.get_json()
     return jsonify({"mensajito": "REGRESO TECXTO XYZ"}), 200
